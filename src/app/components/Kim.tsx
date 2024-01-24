c5d1e9337448ec66dd7cfc5bc546e9c9f5f9c247
rarma4 <rarma4@gmail.com>
2024-01-24 18:03:07 -0300

    kim

--- src/app/components/Kim.tsx
+++ src/app/components/Kim.tsx
@@ -63,51 +63,52 @@ export default function Kim() {
                                     <p>Faixa preta internacional</p>
                                     <p>Hapkido 10º dan</p>
                                     <p>Taekwondo 9º dan</p>
-                                    <p><strong>1943 - </strong>Nasceu a 5 de junho, em seoul, capital da Coréia do Sul.</p>
-                                    <p><strong>1954 - </strong>Começa a treinar Taekwondo e Hapkido.</p>
-                                    <p><strong>1957 </strong>
-                                        <ul>
+                                    <hr></hr>
+                                    <strong>1943</strong><p>Nasceu a 5 de junho, em seoul, capital da Coréia do Sul.</p>
+                                    <strong>1954</strong><p>Começa a treinar Taekwondo e Hapkido.</p>
+                                    <strong>1957</strong>
+                                    <p>                                       <ul>
                                             <li>Recebeu o diploma de faixa preta 1º dan de Hapkido do mestre “YOUNG SOOL CHOI”.</li>
                                             <li>Recebe o diploma de faixa preta 1“ dan de Taekwondo do mestre “HWANG KI”</li>
                                         </ul>
                                     </p>
-                                    <p><strong>1968 </strong>
-                                        <ul>
+                                    <strong>1968</strong>
+                                    <p>                                       <ul>
                                             <li>Inalgura a 1 º academia na Coréia com o nome de ”TCHON YOUNG”, que ensinava Taekwndo e Hapkido junto.</li>
                                             <li>funda a “Bum moo kwan Hapkido”. E obtém a presidência.</li>
                                         </ul>
                                     </p>
-                                    <p><strong>1972 - </strong>Recebeu o diploma como melhor técnica em demonstração, no 1º encontro das artes marciais, promovido pela rede de televisão “MBC – Seoul, Coréia”.</p>
-                                    <p><strong>1974 - </strong>Recebeu o diploma como melhor técnica em demonstração, no 2º encontro das artes marciais, promovido pela rede de televisão “MBC – Seoul, Coréia”.</p>
-                                    <p><strong>1977 - </strong>Chega no Brasil, sendo, faixa preta em Taekwondo 7º dan e Hapkido 8º dan.</p>
-                                    <p><strong>1980 - </strong>Funda a “Associação Kim Taekwodo e Hapkido clube o KIM 1980”.</p>
-                                    <p><strong>1982 - </strong>Recebeu da Federação Pró Taekwondo, na Coréia do Sul, o diploma em prata, o qual certifica que foi encarregado de organizar o Pró Taekwodo no Brasil.</p>
-                                    <p><strong>1982 - </strong>Recebeu da Federação Pró Taekwondo, na Coréia do Sul, o diploma em prata, o qual certifica que foi encarregado de organizar o Pró Taekwodo no Brasil.</p>
-                                    <p><strong>1985 - </strong>Ficou como vice – presidente da Associação Coreana de Taekwondo no Brasil.</p>
-                                    <p><strong>1986 - </strong>Recebeu o diploma de melhor demonstração técnica no encontro das artes marciais, realizado em Manaus, Brasil.</p>
-                                    <p><strong>1991 - </strong>Ficou como Vice – presidente da comissão organizadora do primeiro campeonato mundial de Hapkido, em Seoul, Coréia do Sul.</p>
-                                    <p><strong>1993 </strong>
-                                        <ul>
+                                    <strong>1972</strong><p>Recebeu o diploma como melhor técnica em demonstração, no 1º encontro das artes marciais, promovido pela rede de televisão “MBC – Seoul, Coréia”.</p>
+                                    <strong>1974</strong><p>Recebeu o diploma como melhor técnica em demonstração, no 2º encontro das artes marciais, promovido pela rede de televisão “MBC – Seoul, Coréia”.</p>
+                                    <strong>1977</strong><p>Chega no Brasil, sendo, faixa preta em Taekwondo 7º dan e Hapkido 8º dan.</p>
+                                    <strong>1980</strong><p>Funda a “Associação Kim Taekwodo e Hapkido clube o KIM 1980”.</p>
+                                    <strong>1982</strong><p>Recebeu da Federação Pró Taekwondo, na Coréia do Sul, o diploma em prata, o qual certifica que foi encarregado de organizar o Pró Taekwodo no Brasil.</p>
+                                    <strong>1982</strong><p>Recebeu da Federação Pró Taekwondo, na Coréia do Sul, o diploma em prata, o qual certifica que foi encarregado de organizar o Pró Taekwodo no Brasil.</p>
+                                    <strong>1985</strong><p>Ficou como vice – presidente da Associação Coreana de Taekwondo no Brasil.</p>
+                                    <strong>1986</strong><p>Recebeu o diploma de melhor demonstração técnica no encontro das artes marciais, realizado em Manaus, Brasil.</p>
+                                    <strong>1991</strong><p>Ficou como Vice – presidente da comissão organizadora do primeiro campeonato mundial de Hapkido, em Seoul, Coréia do Sul.</p>
+                                    <strong>1993</strong>
+                                    <p>                                       <ul>
                                             <li>Fundou a Federação de Hapkido do estado de São Paulo, Ocupando o cargo de presidente por votação unânime.</li>
                                             <li>Ficou como Vice – presidente da comissão organizadora do 2 º campeonato mundial de Hapkido, em Seoul, Coréia.</li>
                                             <li>Ganhou a eleição para a presidência, da Associação Coreana de Taekwondo no Brasil.</li>
                                             <li>Ganhou a eleição para a presidência da Associação dos mestres coreanos no Brasil.</li>
                                         </ul>
                                     </p>
-                                    <p><strong>1994 - </strong>Recebeu o diploma de melhor demonstração técnica, no encontro das artes marciais, em Otawa, Canadá.</p>
-                                    <p><strong>2001 - </strong>Fundou a Confederação Brasileira de Hapkido, ocupando a presidência por votação unânime.</p>
-                                    <p><strong>2007 </strong>
-                                        <ul>
+                                    <strong>1994 - </strong><p>Recebeu o diploma de melhor demonstração técnica, no encontro das artes marciais, em Otawa, Canadá.</p>
+                                    <strong>2001</strong><p>Fundou a Confederação Brasileira de Hapkido, ocupando a presidência por votação unânime.</p>
+                                    <strong>2007</strong>
+                                    <p>                                       <ul>
                                             <li>Ganhou a eleição para a presidência da Associação Coreana de Taekwondo no Brasil.</li>
                                             <li> Ganhou a eleição para a presidência da Associação dos mestres coreanos do Brasil.</li>
 
                                         </ul>
                                     </p>
                                     <hr></hr>
-                                    <p><strong>1983 à 2007 - </strong>Realizou da 1º à 32º Copa Grão mestre YUN SIK KIM de Taekwondo.</p>
-                                    <p><strong>1993 à 2007 - </strong>Realizou do” 1º ao 14º Campeonato Paulista de Hapkido”. E do “1º ao 14 º Campeonato Brasileiro de Hapkido.</p>
-                                    <p><strong>1975 á 2007 - </strong>Ensinou em seminários de Taekwondo e Hapkido em mais de 40 países</p>
-                                    <p><strong>1968 á 2007 - </strong>Formou mais de 30 mil faixas pretas de 1º dan ao 9º dan de Taekwondo e Hapkido em mais de 50 países.</p>
+                                    <strong>1983 à 2007</strong><p>Realizou da 1º à 32º Copa Grão mestre YUN SIK KIM de Taekwondo.</p>
+                                    <strong>1993 à 2007</strong><p>Realizou do” 1º ao 14º Campeonato Paulista de Hapkido”. E do “1º ao 14 º Campeonato Brasileiro de Hapkido.</p>
+                                    <strong>1975 á 2007</strong><p>Ensinou em seminários de Taekwondo e Hapkido em mais de 40 países</p>
+                                    <strong>1968 á 2007</strong><p>Formou mais de 30 mil faixas pretas de 1º dan ao 9º dan de Taekwondo e Hapkido em mais de 50 países.</p>
                                 </div>
                             </div>
                         </div>
