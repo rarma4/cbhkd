f79aa93b6c038967f56762e7d675890fbff33fba
rarma4 <rarma4@gmail.com>
2024-01-26 23:20:59 -0300

    init contato

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import '../styles/pages/page.scss'
+import HeaderContato from '../sections/HeaderContato'
+import QuemPodeTreinar from '../sections/QuemPodeTreinar'
+import Contato from '../sections/Contato'
+import Regras from '../sections/Regras'
+import Estudo from '../sections/Estudo'
+import BannerInferior from '../sections/BannerInferior'
+
+export default function Graduacao() {
+  return (
+    <>
+      <HeaderContato/>
+      <Contato/>
+      <BannerInferior/>
+    </>
+  )
+}
