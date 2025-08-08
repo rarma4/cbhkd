b1706825cd5bb028ee398eb6a26df1a203275917
rarma4 <rarma4@gmail.com>
2025-08-07 22:31:14 -0300

    update for

--- src/app/sections/BannerInferiorContato.tsx
+++ src/app/sections/BannerInferiorContato.tsx
@@ -4,7 +4,7 @@ export default function BannerInferiorContato() {
   return (
     <>
       <div className="container-fluid bg-primary text-white d-flex justify-content-evenly flex-column" id="bannerInferiorContato">
-        <div className="container-fluid">
+        <div className="container-fluid" >
           <div className="row text-center">
             <div className="col-lg-12 col-md-12 text-center  colcenter d-flex align-items-center">
             </div>
